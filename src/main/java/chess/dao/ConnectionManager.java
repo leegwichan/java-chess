21903e7631133767152206a3ccaeb685c17723a9
leegwichan <cnddkscndgus@naver.com>
2024-04-07 08:31:10 +0900

    refactor(ConnectionManager): 데이터베이스 설정값 분리

--- src/main/java/chess/dao/ConnectionManager.java
+++ src/main/java/chess/dao/ConnectionManager.java
@@ -1,18 +1,17 @@
 package chess.dao;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
 
-    private static final String SERVER = "localhost:13306";
-    private static final String DATABASE = "chess";
-    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-
-    private static final String URL = "jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION;
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private static final String FILE_LOCATION = "src/main/resources/mysql.properties";
+    private static final Properties PROPERTIES = loadProperties();
 
     private final Connection singleConnection;
 
@@ -20,9 +19,24 @@ public class ConnectionManager {
         singleConnection = createConnection();
     }
 
-    private static Connection createConnection() {
+    private static Properties loadProperties() {
+        try (FileReader mysqlSettingFile = new FileReader(FILE_LOCATION)) {
+            Properties properties = new Properties();
+            properties.load(mysqlSettingFile);
+            return properties;
+        } catch (FileNotFoundException e) {
+            throw new IllegalStateException("mysql 설정 파일이 존재하지 않습니다.");
+        } catch (IOException e) {
+            throw new IllegalStateException("파일 입출력 오류가 발생했습니다.");
+        }
+    }
+
+    private Connection createConnection() {
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            String url = PROPERTIES.getProperty("URL");
+            String username = PROPERTIES.getProperty("USERNAME");
+            String password = PROPERTIES.getProperty("PASSWORD");
+            return DriverManager.getConnection(url, username, password);
         } catch (final SQLException e) {
             System.err.println("DB 연결 오류:" + e.getMessage());
             throw new IllegalStateException("DB와 연결할 수 없습니다.", e);
